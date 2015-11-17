32af54da14708d51225cd99328aa40fe987a0711
Todd Hayes <todd@i.tv>
2015-11-17 15:05:34 -0700

    change to config file

--- server/config.js
+++ server/config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	PORT: process.env.PORT || 8001,
-	MONGO_URI: process.env.MONGO_LABS_URI || 'mongodb://localhost:27017/emily_hayes_homes'
+	MONGO_URI: process.env.MONGO_LABS_URI || "mongodb://localhost:27017/emily_hayes_homes"
 };
+
+
+
