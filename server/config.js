9f2700072915717a4917ac654d95f73e4a3a5046
Todd Hayes <todd@i.tv>
2015-11-17 14:53:57 -0700

    change got config

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	PORT: process.env.PORT || 8001,
-	MONGO_URI: 'mongodb://<dbuser>:<dbpassword>@ds055584.mongolab.com:55584/emilyhayeshomes' || 'mongodb://localhost:27017/emily_hayes_homes'
+	MONGO_URI: process.env.MONGO_LABS_URI || 'mongodb://localhost:27017/emily_hayes_homes'
 };
